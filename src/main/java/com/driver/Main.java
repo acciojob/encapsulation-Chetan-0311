10116fea9bb712b38f8b4b8c7dc856f1ea5e13f8
Chetan-0311 <cjangra740@gmail.com>
2022-10-09 15:10:19 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+//  obj.name = "Name";
+     obj.setName("AccioJob");
+     System.out.println(obj.getName());
+  }
 }
